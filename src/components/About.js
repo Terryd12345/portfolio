8ad2bb82152e641f94610e97ce51b663e20ce783
Terence Denning <terencedenning@outlook.com>
2020-02-24 17:33:36 +1100

    add additional pages

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div id="about" className="about">
+            About
+        </div>
+    )
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,19 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import React from "react";
+import { Helmet } from 'react-helmet';
+import Home from "../components/Home";
+import About from '../components/About';
+import Projects from "../components/Projects";
+
 
 export default () => (
   <React.Fragment>
-    <div className="home">
-      <h1 className="home__header">Terence Denning</h1>
-      <h2 className="home__subheader">Software Engineer</h2>
-      <div className="home__social">
-        <a
-          href="https://www.linkedin.com/in/terence-denning-0a89a7122/"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a href="https://github.com/tden123" target="_blank">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </div>
-      <div className="home__navigation">
-        <span>About</span>
-        <span>Projects</span>
-        <span>Contact</span>
-      </div>
-    </div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Terence Denning</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
+    <Home />
+    <About />
+    <Projects />
   </React.Fragment>
 )
