12c2f91f24d9c6e0ef480472bb6caef72372eecb
Terence Denning <terrencedenning@gmail.com>
2020-03-27 18:19:38 +1100

    restyle homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,32 +5,30 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 const Home = () => {
   return (
     <section className="home">
-      <h1 className="home__header">Terence Denning</h1>
-      <h2 className="home__subheader">I build software for the web</h2>
-      <div className="home__social">
-        <a
-          href="https://www.linkedin.com/in/terence-denning-0a89a7122/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon
-            className="home__social--linkedin"
-            icon={faLinkedin}
-          />
-        </a>
-        <a
-          href="https://github.com/tden123"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon className="home__social--github" icon={faGithub} />
-        </a>
-      </div>
-      <div className="home__navigation">
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
-      </div>
+      <article className="home__content">
+        <h1 className="home__header">Terence Denning</h1>
+        <h2 className="home__subheader">I build software for the web</h2>
+        <h3 className="home__location">Melbourne, AU</h3>
+        <div className="home__social">
+          <a
+            href="https://www.linkedin.com/in/terence-denning-0a89a7122/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              className="home__social--linkedin"
+              icon={faLinkedin}
+            />
+          </a>
+          <a
+            href="https://github.com/tden123"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className="home__social--github" icon={faGithub} />
+          </a>
+        </div>
+      </article>
     </section>
   )
 }
