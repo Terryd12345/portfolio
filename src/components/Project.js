4507acade9b38b182ae639f7af57117504e77fc7
Terence Denning <terencedenning@outlook.com>
2020-05-01 20:22:14 +1000

    project overlay and navbar highlights

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 const Project = (props) => {
     const { image, description, title } = props;
     return (
         <div className='project'>
             <div className="project__image">
+                <div className="project__image-overlay">
+                    <div className="project__image-overlay--icons">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </div>
+                </div>
                 <img src={image} alt="project"/>
             </div>
             <h3 className="project__title">{title}</h3>
