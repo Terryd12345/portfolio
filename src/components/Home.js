03a7013b21e40b33d4981fdac9895cf751a2f559
Terence Denning <terencedenning@outlook.com>
2020-05-16 18:13:47 +1000

    remove hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Particles from 'react-particles-js'
-import windowSize from '../hooks/windowSize'
 
 const Home = () => {
     
-    const { width } = windowSize();
+    
 
     return (
         <section id="home" className="home">
@@ -22,90 +21,48 @@ const Home = () => {
                     </a>
                 </div>     
             </div>
-            { width > 600 ? (
+           
                 <Particles 
                 params={{
-                      particles: {
-                          number: {
-                              value: 10,
-                              density: {
-                                  enable: true,
-                                  value_area: 1000
-                              }
-                          },
-                          line_linked: {
-                              shadow: {
-                                  enable: true,
-                                  color: "#FF9B21",
-                                  blur: 1
-                              }
-                          },
-                          shape: {
-                              type: 'circle',
-                              stroke: {
-                                  width: 10,
-                                  color: '#FF9B21'
-                              }
-                          },
-                          move: {
-                              enable: true,
-                              speed: 4,
-                              direction: 'none',
-                              random: false,
-                              straight: false,
-                              out_mode: 'out',
-                              bounce: false,
-                              attract: {
-                                  enable: false,
-                                  rotateX: 0,
-                                  rotateY: 0
-                              }
-                          }
-                      }
-                  }}
-              />
-            ) : (
-                <Particles 
-              params={{
-            		particles: {
-                        number: {
-                            value: 2,
-                            density: {
+                        particles: {
+                            number: {
+                                value: 5,
+                                density: {
+                                    enable: true,
+                                    value_area: 1000
+                                }
+                            },
+                            line_linked: {
+                                shadow: {
+                                    enable: true,
+                                    color: "#FF9B21",
+                                    blur: 1
+                                }
+                            },
+                            shape: {
+                                type: 'circle',
+                                stroke: {
+                                    width: 10,
+                                    color: '#FF9B21'
+                                }
+                            },
+                            move: {
                                 enable: true,
-                                value_area: 1000
-                            }
-                        },
-            			line_linked: {
-            				shadow: {
-            					enable: true,
-            					color: "#FF9B21",
-            					blur: 1
-            				}
-                        },
-                        shape: {
-                            type: 'circle',
-                            stroke: {
-                                width: 10,
-                                color: '#FF9B21'
-                            }
-                        },
-                        move: {
-                            enable: true,
-                            speed: 4,
-                            direction: 'none',
-                            random: false,
-                            straight: false,
-                            out_mode: 'out',
-                            bounce: false,
-                            attract: {
-                                enable: false,
-                                rotateX: 0,
-                                rotateY: 0
+                                speed: 6,
+                                direction: 'none',
+                                random: false,
+                                straight: false,
+                                out_mode: 'out',
+                                bounce: false,
+                                attract: {
+                                    enable: false,
+                                    rotateX: 0,
+                                    rotateY: 0
+                                }
                             }
                         }
-            		}
-            	}}
-            />
+                    }}
+                />
             )}
             
         </section>
