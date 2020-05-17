b51fbcf4c0d1584e5134187144123c0a6b55cf44
Terence Denning <terencedenning@outlook.com>
2020-05-17 13:08:40 +1000

    align about section

--- src/components/WhyMe.js
+++ src/components/WhyMe.js
@@ -3,8 +3,10 @@ import React from 'react'
 const WhyMe = () => {
     return (
         <section id="whyme" className="whyme">
-            <h3 className="whyme--title section-header">How I Work</h3>
-            <p className="whyme--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eum, esse placeat laboriosam totam officia consequuntur! Inventore corporis nam sint laborum iste voluptatem veniam aliquam, quisquam saepe! Dolore eum fugiat, autem fuga placeat repellat minima facilis ducimus delectus nulla nihil quod aspernatur necessitatibus omnis quis dignissimos ipsa modi maxime excepturi corporis deserunt distinctio ex! Vel in omnis quia rerum, voluptatum ratione! Pariatur minima ea repellat alias doloremque molestias rerum aliquam reiciendis? Quos incidunt impedit obcaecati distinctio asperiores hic provident. Omnis repellat commodi veniam error, quas officiis totam autem illo similique iure ad soluta aspernatur enim optio aliquam placeat. Possimus, pariatur!</p>
+            <div className="whyme__content">
+                <h3 className="whyme--title section-header">How I Work</h3>
+                <p className="whyme--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eum, esse placeat laboriosam totam officia consequuntur! Inventore corporis nam sint laborum iste voluptatem veniam aliquam, quisquam saepe! Dolore eum fugiat, autem fuga placeat repellat minima facilis ducimus delectus nulla nihil quod aspernatur necessitatibus omnis quis dignissimos ipsa modi maxime excepturi corporis deserunt distinctio ex! Vel in omnis quia rerum, voluptatum ratione! Pariatur minima ea repellat alias doloremque molestias rerum aliquam reiciendis? Quos incidunt impedit obcaecati distinctio asperiores hic provident. Omnis repellat commodi veniam error, quas officiis totam autem illo similique iure ad soluta aspernatur enim optio aliquam placeat. Possimus, pariatur!</p>
+            </div>
         </section>
     )
 }
