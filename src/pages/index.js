614a3385c41c66f910fc9c49402a3509b6de3577
Terence Denning <terencedenning@outlook.com>
2021-05-09 16:42:07 +1000

    fix vulnerabilties and comment out contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Home from "../components/Home"
 import Services from "../components/Services"
 import Projects from "../components/Projects"
-import Contact from "../components/Contact"
 import Navbar from "../components/Navbar"
 import About from "../components/About"
 import Footer from "../components/Footer"
@@ -25,7 +24,6 @@ export default () => {
         <Services />
         <Projects />
         <About />
-        <Contact />
         <Footer />
       </main>
     </>
